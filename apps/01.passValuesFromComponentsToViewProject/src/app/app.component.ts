46fde4999fe8fb06258ff2bc3d63f29092b851bd
Evdokia Yordanova <oki4102@abv.bg>
2018-09-09 23:50:35 +0300

    Finish 01Project - give some values from component to view

--- apps/01.passValuesFromComponentsToViewProject/src/app/app.component.ts
+++ apps/01.passValuesFromComponentsToViewProject/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'passValuesFromComponentsToViewProject';
+export class AppComponent implements OnInit {
+  age: number;
+  fullName: string;
+  status: boolean;
+  email: any;
+  title = 'Pass Values from Components to View';
+
+  ngOnInit() {
+    this.age = 20;
+    this.fullName = 'Okiana';
+    this.status = true;
+    this.email = 'oki@gmail.com';
+  }
 }
