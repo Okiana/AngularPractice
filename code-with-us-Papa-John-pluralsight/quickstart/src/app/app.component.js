65d23fe0ed458eac6ab62077d1e7f03622bdee15
Evdokia Yordanova <oki4102@abv.bg>
2019-01-23 01:31:59 +0200

    refactor - separating to different files

--- code-with-us-Papa-John-pluralsight/quickstart/src/app/app.component.js
+++ code-with-us-Papa-John-pluralsight/quickstart/src/app/app.component.js
@@ -24,8 +24,10 @@ var AppComponent = (function () {
 }());
 AppComponent = __decorate([
     core_1.Component({
+        moduleId: module.id,
         selector: 'my-app',
-        template: "\n      <h1>{{customer.name}}</h1>\n      <p><i> {{customer.name}} is on the {{customer.address.street}} street in {{customer.address.city}} city \n      in region {{customer.address.region}} in state {{customer.address.state}}</i></p>\n      <br/>\n\n      <fieldset>\n      <label>Name: </label> <input [(ngModel)]=\"customer.name\"/>\n      </fieldset>\n    \n      <label><input type=\"checkbox\" [(ngModel)]=\"hideAddress\" />Show/Hide Address</label> \n      <div [hidden]=\"hideAddress\">\n        <fieldset>\n        <label>Street: </label> <input [(ngModel)]=\"customer.address.street\"/>\n        </fieldset>\n        <fieldset>\n        <label>City: </label> <input [(ngModel)]=\"customer.address.city\"/>\n        </fieldset>\n        <fieldset>\n        <label>State: </label>\n        <select [(ngModel)]=\"customer.address.state\">\n        <option>California</option>\n        <option>Kolorado</option>\n        <option>Quebek</option>\n      </select> \n      </fieldset>\n        <fieldset>\n          <label>Region: </label>\n          <select [(ngModel)]=\"customer.address.region\">\n          <option>East</option>\n          <option>North</option>\n          <option>West</option>\n          <option>South</option>\n        </select> \n        </fieldset>\n      </div>\n    ",
+        templateUrl: 'app.component.html',
+        styleUrls: ['app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- code-with-us-Papa-John-pluralsight/quickstart/src/app/app.component.ts
+++ code-with-us-Papa-John-pluralsight/quickstart/src/app/app.component.ts
@@ -3,44 +3,11 @@ import { Component } from '@angular/core';
 import { Customer } from './module'
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-      <h1>{{customer.name}}</h1>
-      <p><i> {{customer.name}} is on the {{customer.address.street}} street in {{customer.address.city}} city 
-      in region {{customer.address.region}} in state {{customer.address.state}}</i></p>
-      <br/>
+  templateUrl: 'app.component.html',
+  styleUrls:['app.component.css']
 
-      <fieldset>
-      <label>Name: </label> <input [(ngModel)]="customer.name"/>
-      </fieldset>
-    
-      <label><input type="checkbox" [(ngModel)]="hideAddress" />Show/Hide Address</label> 
-      <div [hidden]="hideAddress">
-        <fieldset>
-        <label>Street: </label> <input [(ngModel)]="customer.address.street"/>
-        </fieldset>
-        <fieldset>
-        <label>City: </label> <input [(ngModel)]="customer.address.city"/>
-        </fieldset>
-        <fieldset>
-        <label>State: </label>
-        <select [(ngModel)]="customer.address.state">
-        <option>California</option>
-        <option>Kolorado</option>
-        <option>Quebek</option>
-      </select> 
-      </fieldset>
-        <fieldset>
-          <label>Region: </label>
-          <select [(ngModel)]="customer.address.region">
-          <option>East</option>
-          <option>North</option>
-          <option>West</option>
-          <option>South</option>
-        </select> 
-        </fieldset>
-      </div>
-    `,
 })
 export class AppComponent  { 
   
