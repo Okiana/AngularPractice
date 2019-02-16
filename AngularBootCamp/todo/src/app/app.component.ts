e6e6890711ee2180f15a4cbd6d3674b2fa2ddb07
Evdokia Yordanova <oki4102@abv.bg>
2019-02-16 11:00:24 +0200

    Basic list display of todos

--- AngularBootCamp/todo/src/app/app.component.ts
+++ AngularBootCamp/todo/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+    todos = [];
+    addHandler(value: string) {
+        this.todos.push(value);
+    }
 }
